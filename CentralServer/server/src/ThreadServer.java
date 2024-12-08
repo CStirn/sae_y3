d3985e52803744432a86db07bcc7957d233aa338
CStirn <cedric.stirnemann@gmail.com>
2024-12-08 13:30:18 +0100

    Added a first version for the TCP Server. To explain how it works : - ThreadServer.java contains the method to process requests. It splits the request properly, and then, based on the idReq, it call a specific function, passing its args as a parameter. This function will then process these args for this specific request, and return the response. The response is then written at the end of the main function (requestLoop)

--- CentralServer/server/src/ThreadServer.java
+++ CentralServer/server/src/ThreadServer.java
@@ -35,9 +35,8 @@ class ThreadServer extends Thread {
 
 	public void requestLoop() {
 
-		String req = "";
-		String idReq;
-		String[] reqParts;
+		String req = "", res = "", idReq;
+		String[] reqParts, argsReq;
 
 		try {
 			while(true) {
@@ -46,18 +45,32 @@ class ThreadServer extends Thread {
 					break;
 				}
 
-				reqParts = req.split(" ");
+				reqParts = req.split(":");  // Split between the request name and the args
 				idReq = reqParts[0];
+				argsReq = req.split(",");
+				for (int i = 0; i < argsReq.length; i++) {
+					argsReq[i] = argsReq[i].trim();
+				}
 
-				if ("AUTOREGISTER".equals(idReq)) {
-					requestAutoRegister(reqParts);
+				// Recieving measurements from the drone
+				if ("DRONE.SendMeasurement".equals(idReq)) {
+					res = recieveSensorMeasurement(argsReq);
 				}
+
 				else if ("STOREMEASURE".equals(idReq)) {
-					requestStoreMeasure(reqParts);
+					// TODO
 				}
+
 				else if ("STOREANALYSIS".equals(idReq)) {
-					requestStoreAnalysis(reqParts);
+					// TODO
 				}
+
+				// Invalid request
+				else {
+					res = "ERR Invalid request name";
+				}
+
+				ps.println(res);
 			}
 			System.out.println("end of request loop");
 		}
@@ -66,52 +79,34 @@ class ThreadServer extends Thread {
 		}
 	}
 
-	protected void requestAutoRegister(String[] params) throws IOException {
+	protected String recieveSensorMeasurement(String[] args) throws IOException
+	{
 		// remove the identifier+uc from params
-		List<String> lst = new ArrayList<>();
-		for(int i=2;i<params.length;i++) {
-			lst.add(params[i]);
+		if (args.length == 0) {
+			return "ERR No data provided";
 		}
-		System.out.println("processing request AUTO REGISTER");
-		if (params.length < 3) {
-			ps.println("ERR invalid number of parameters");
-			return;
+
+		// Range sensor (left)
+		if ("RangeSensorLeft".equals(args[0])) {
+			if (args.length != 2) {
+				return "ERR Invalid measurement provided";
+			}
+
+			try {
+				float measurement = Float.parseFloat(args[1]);
+				System.out.println("Measurement for 'Range sensor (left)': " + measurement);
+			} catch (NumberFormatException e) {
+				return "ERR Invalid measurement provided";
+			}
 		}
-		// (un)comment to choose direct mongo access or through the node API
-		//String answer = exchanger.getMongoDriver().autoRegisterModule(params[1], lst);
-		String answer = exchanger.getHttpDriver().autoRegisterModule(params[1], lst);
-		System.out.println(answer);
-		ps.println(answer);
+
+		// Invalid/unhandled sensor
+		else {
+			return "ERR Invalid sensor";
+		}
+
+		return "ERR Unknown";
 	}
-
-	protected void requestStoreMeasure(String[] params) throws IOException {
-		System.out.println("processing request STORE MEASURE");
-
-		if (params.length != 5) {
-			ps.println("ERR invalid number of parameters");
-			return;
-		}
-		// (un)comment to choose direct mongo access or through the node API
-		//String answer = exchanger.getMongoDriver().saveMeasure(params[1], params[2], params[3], params[4]);
-		String answer = exchanger.getHttpDriver().saveMeasure(params[1], params[2], params[3], params[4]);
-		System.out.println(answer);
-		ps.println(answer);
-	}
-
-	protected void requestStoreAnalysis(String[] params) throws IOException {
-		System.out.println("processing request STORE ANALYSIS");
-
-		if (params.length != 4) {
-			ps.println("ERR invalid number of parameters");
-			return;
-		}
-		// (un)comment to choose direct mongo access or through the node API
-		String answer = exchanger.getMongoDriver().saveAnalysis(params[1], params[2], params[3]);
-		//String answer = exchanger.getHttpDriver().saveAnalysis(params[1], params[2], params[3]);
-		System.out.println(answer);
-		ps.println(answer);
-	}
-
 }
 
 		
